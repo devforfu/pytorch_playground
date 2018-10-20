028a670715f74365c57067598785ea07d9be18d3
devforfu <developer.z@outlook.com>
2018-10-20 10:23:16 +0500

    Cosine annealing scheduler

--- text_classification/main_imdb.py
+++ text_classification/main_imdb.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 
+import fire
 from fastai.data import DataBunch
 from fastai.text import RNNLearner
 from fastai.train import TrainingPhase, annealing_cos
 from fastai.callbacks import GeneralScheduler
+from fastai.callbacks.tracker import EarlyStopping, SaveModel
 from fastai.text.data import TextLMDataBunch
+from torch.nn import functional as F
 
 from dataset import IMDB
 from tokenizer import Vocab, tokenize_in_parallel
@@ -19,31 +22,56 @@ TEST_PATH = DATA_ROOT / 'test'
 
 
 def main():
+    fire.Fire(train_lm)
+
+
+def train_lm(n_cycles: int=3, cycle_len: int=1, cycle_mult: int=2,
+             momentum: float=0.8, bptt: int=40, lr: float=1e-3,
+             wd: float=1e-7):
+
     datasets = create_or_restore(DATA_ROOT)
-    lm_data = [datasets['train_unsup'], datasets['test_unsup']]
-    bunch = TextLMDataBunch.create(lm_data, path=LM_PATH)
+    lm_data = [
+        fastai_patch(ds) for ds in (
+            datasets['train_unsup'], datasets['test_unsup'])]
+    bunch = TextLMDataBunch.create(lm_data, path=LM_PATH, bptt=bptt)
 
-    lr = 1e-3
-    wd = 1e-7
-    cycle_len = 1
-    n_cycles = 3
-    cycle_mult = 2
     n = sum(len(ds) for ds in lm_data)
-
     phases = [
         TrainingPhase(
-            length=n*(cycle_len*cycle_mult),
-            lrs=lr, moms=0.9, lr_anneal=annealing_cos)
-        for _ in range(n_cycles)]
+            n*(cycle_len * cycle_mult**i),
+            lrs=lr, moms=momentum, lr_anneal=annealing_cos
+        ) for i in range(n_cycles)]
+    learner = RNNLearner.language_model(bunch, bptt)
+    cbs = [
+        EarlyStopping(learner, patience=2),
+        GeneralScheduler(learner, phases),
+        SaveModel(learner)]
 
-    learner = RNNLearner.language_model(bunch)
-    sched = GeneralScheduler(learner, phases)
-    learner.fit(1, lr/2, wd=wd, callbacks=[sched])
+    if cycle_mult == 1:
+        total_epochs = n_cycles * cycle_len
+    else:
+        total_epochs = int(cycle_len*(1 - cycle_mult**n_cycles)/(1 - cycle_mult))
 
-    print('Saving model')
-    learner.save('lm')
-    learner.save_encoder('lm_enc')
+    print(f'Total number of epochs: {total_epochs:d}')
+    try:
+        learner.fit(total_epochs, wd=wd, callbacks=cbs)
+    except RuntimeError as e:
+        print(f'Model training error: {e}')
+    finally:
+        folder = learner.path/learner.model_dir
+        print(f'Saving latest model state into {folder}')
+        learner.save('lm_final')
+        learner.save_encoder('lm_final_enc')
 
+
+def fastai_patch(ds):
+    """Adding properties to the dataset required to be compatible with
+    fastai library.
+    """
+    ds.__dict__['ids'] = ds.train_data if ds.train else ds.test_data
+    ds.__dict__['vocab_size'] = ds.vocab.size
+    ds.__dict__['loss_func'] = F.cross_entropy
+    return ds
 
 
 def create_or_restore(path: Path):
